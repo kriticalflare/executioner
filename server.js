f1b943bcba592d9dcc271e2440cfd0e53f737e8c
kriticalflare <krithiksuvarna@gmail.com>
2021-11-01 15:00:21 +0530

    Switch to logging domain

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./Config/app');
+const app = require("./Config/app");
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port , ()=>{
-    console.log(`App running on port http://localhost:${port}`);
-})
+const server = app.listen(port, () => {
+  console.log(`App running on port ${process.env.DOMAIN}`);
+});
